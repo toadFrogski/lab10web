cb0c2189c428af1a9b585cb257428bfa716de71d
toadski <toad.frogski@gmail.com>
2023-04-27 00:10:57 +0300

    add some memes 2

--- webapp/controllers/record.php
+++ webapp/controllers/record.php
@@ -6,7 +6,7 @@ if (empty($parent_id)) {
     $query = "INSERT into parent(parent_name, parent_email) values('{$_POST['name']}', '{$_POST['email']}')";
     $conn->query($query);
     $query = "SELECT parent_id from parent where parent_name='{$_POST['name']}' and parent_email = '{$_POST['email']}'";
-    $parent_id = $conn->query($query)->fetch_all();
+    $parent_id = $conn->query($query)->fetch_array();
 }
 $query = "SELECT child_id from child where child_name = '{$_POST['child_name']}' and child_birth = '{$_POST['date']}' and parent_id = '" . (int) $parent_id[0] . "'";
 $child_id = $conn->query($query)->fetch_all();
