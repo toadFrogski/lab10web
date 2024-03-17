02a894fc2933e109608ea2a292b308c868c0c2b8
toadski <toad.frogski@gmail.com>
2024-03-17 17:23:38 +0200

    wip

--- webapp/autoload.php
+++ webapp/autoload.php
@@ -4,7 +4,7 @@ spl_autoload_register(function ($class) {
     // Define the base directory for the root namespace
     $base_dir = __DIR__ . DIRECTORY_SEPARATOR;
     // Replace namespace separators with directory separators
-    $file = $base_dir . str_replace('\\', '/', $class) . '.php';
+    $file = $base_dir . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
 
     if (file_exists($file)) {
         require_once $file;
--- webapp/index.php
+++ webapp/index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-set_include_path(__DIR__ . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'Templates');
+set_include_path(__DIR__ . DIRECTORY_SEPARATOR . 'App' . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'Templates');
 define('BASEDIR', __DIR__);
 
 require_once 'autoload.php';
-require_once(__DIR__ . DIRECTORY_SEPARATOR . "app" . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "Settings.php");
-require_once(__DIR__ . DIRECTORY_SEPARATOR . "app" . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "Routing.php");
+require_once(__DIR__ . DIRECTORY_SEPARATOR . "App" . DIRECTORY_SEPARATOR . "Config" . DIRECTORY_SEPARATOR . "Settings.php");
+require_once(__DIR__ . DIRECTORY_SEPARATOR . "App" . DIRECTORY_SEPARATOR . "Config" . DIRECTORY_SEPARATOR . "Routing.php");
